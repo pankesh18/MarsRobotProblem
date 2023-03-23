2db514b509ed23bf939235bfd4d189fe190f2a46
pankesh18 <wadekar.pankesh@gmail.com>
2023-03-23 21:42:31 +0000

    Inputs taken

--- MarsPlateauRobot/Program.cs
+++ MarsPlateauRobot/Program.cs
@@ -4,7 +4,13 @@
     {
         public static void Main(string[] args)
         {
+            string GridSize;
+            string Nav_Intruct;
             Console.WriteLine("Enter grid size of the Mars Plateau (Example 3x3):");
+            GridSize= Console.ReadLine();
+            Console.WriteLine("Enter nagigation instructions (Example RLRLFL)");
+            Nav_Intruct= Console.ReadLine();
+
 
 
         }   
